a801f1c0a425d07c7d16956837128b96e2f1be85
Dhruvpatel <dhruvpatel22021998@gmail.com>
2022-10-26 21:39:07 -0400

    Login And Registration Page

--- app/src/main/java/com/dvp/expensemanagement/MainActivity.java
+++ app/src/main/java/com/dvp/expensemanagement/MainActivity.java
@@ -2,13 +2,32 @@ package com.dvp.expensemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView btn_reg = (TextView) findViewById(R.id.create_account);
+
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegistrationActivity.class);
+                startActivity(intent);
+
+                
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/dvp/expensemanagement/RegistrationActivity.java
+++ app/src/main/java/com/dvp/expensemanagement/RegistrationActivity.java
@@ -2,7 +2,10 @@ package com.dvp.expensemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        TextView btn_login = (TextView) findViewById(R.id.signin_here);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this,MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
