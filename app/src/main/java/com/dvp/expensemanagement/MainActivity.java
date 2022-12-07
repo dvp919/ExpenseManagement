ad7f686b17259c6602e4023626b9df4b980c1929
Saumya <saumya11mehta@gmail.com>
2022-12-07 07:54:16 -0500

    Added Navigation

--- app/src/main/java/com/dvp/expensemanagement/MainActivity.java
+++ app/src/main/java/com/dvp/expensemanagement/MainActivity.java
@@ -6,15 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnLogin;
+    private TextView mForgotPassword;
+    private TextView mSignupHere;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        }
 
         TextView btn_reg = (TextView) findViewById(R.id.create_account);
 
